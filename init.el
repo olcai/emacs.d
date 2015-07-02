2a3084cc4a15e54d47b6c0e857d57eee10d30117
Erik Timan <dev@timan.info>
2015-07-02 22:05:29 +0200

    Fix URL to MELPA

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
